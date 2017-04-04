28fb564de012a11184ace47a07202d109fcce666
MarioRoncador <mroncador@alaska.edu>
2017-04-03 22:14:30 -0400

    Assignment 26 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
 
   def avatar_url(current_user)
     gravatar_id = Digest::MD5::hexdigest(current_user.email).downcase
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    puts current_user.email
+    puts gravatar_id
+    puts "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
     return "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
   end
 end
